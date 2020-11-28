1d5a93af6c5438efce71c6e552c62c3ad403bd1a
xd003 <singhbadal04@gmail.com>
2020-11-28 08:02:19 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#=============================================================
+# https://github.com/xd003/clone
+# File Name: setup.sh
+# Author: xd003
+# Description: Installing prerequisites for clone script
+# System Required: Any Linux based distribution
+#=============================================================
