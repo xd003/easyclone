9a4f1301b62e49a0b6db9d9e40f39b7d88db7d39
xd003 <singhbadal04@gmail.com>
2020-12-01 11:16:41 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -38,7 +38,7 @@ elif [ "$arch" == "x86_64" ] ; then
   arch=amd64
 fi
 
-dl=https://github.com/mawaya/rclone/releases/download/fclone-$version/fclone-$version-linux-$arch.zip
+wget https://github.com/mawaya/rclone/releases/download/fclone-$version/fclone-$version-linux-$arch.zip
 
 mkdir $HOME/easyclone
 mkdir $HOME/.easyclone
