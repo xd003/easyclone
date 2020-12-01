6727392419ec65d8e295d849bf72c132b3225f62
xd003 <singhbadal04@gmail.com>
2020-12-01 10:42:03 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,3 +9,11 @@
 
 version=v0.4.1
 arch="$(uname -m)"
+
+if [ "$arch" == "arm64" ] || [ "$arch" == "aarch64" ] ; then
+  arch=arm64
+elif [ "$arch" == "x86_64" ] ; then
+  arch=amd64
+fi
+
+dl=https://github.com/mawaya/rclone/releases/download/fclone-$version/fclone-$version-linux-$arch.zip
