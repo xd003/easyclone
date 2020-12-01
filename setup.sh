7d23666cf9d3c7d750bcb465af075d1a0db50445
xd003 <singhbadal04@gmail.com>
2020-12-01 13:40:12 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -85,7 +85,7 @@ fi
 echo && cecho r "Downloading the service accounts from your private repo"
 read -e -p "Input your github username : " username
 read -e -p "Input your github password : " password
-git clone https://"$username":"$Password"@github.com/"$username"/accounts $HOME/easyclone
+git clone https://"$username":"$Password"@github.com/"$username"/accounts $HOME/.easyclone
 cecho b "Service accounts were added Successfully"
 
 # Creating the rclone.conf with appropriate info
