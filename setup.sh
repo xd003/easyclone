3ab6cdeff619561c685c0310f24e976a562dfe74
xd003 <singhbadal04@gmail.com>
2020-12-01 12:01:30 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,23 @@
 # System Supported: Arch , Ubuntu/Debian , Fedora & Termux
 #=============================================================
 
+cecho() {
+        local code="\033["
+        case "$1" in
+                black  | bk) color="${code}0;30m";;
+                red    |  r) color="${code}1;31m";;
+                green  |  g) color="${code}1;32m";;
+                yellow |  y) color="${code}1;33m";;
+                blue   |  b) color="${code}1;34m";;
+                purple |  p) color="${code}1;35m";;
+                cyan   |  c) color="${code}1;36m";;
+                gray   | gr) color="${code}0;37m";;
+                *) local text="$1"
+        esac
+        [ -z "$text" ] && local text="$color$2${code}0m"
+        echo -e "$text"
+}
+
 #Variables 
 version=v0.4.1
 arch="$(uname -m)"
@@ -64,3 +81,10 @@ else
     echo 'export PATH="$PATH:$HOME/.easyclone"' >> $HOME/.bashrc && \
     source ~/.bashrc
 fi
+
+# Pulling the accounts folder containing service accounts from github 
+echo && cecho r "Downloading the service accounts from your private repo"
+read -e -p "Input your github username : " username
+read -e -p "Input your github password : " password
+git clone https://"$username":"$Password"@github.com/"$username"/accounts $HOME/easyclone
+cecho b "Service accounts were added Successfully"
