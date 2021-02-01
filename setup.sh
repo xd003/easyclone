e99936d20d79aa30d14281fc8d8ce10f50e16129
xd003 <singhbadal04@gmail.com>
2021-02-01 09:09:54 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -79,8 +79,8 @@ sudo chmod u+x $spath/clone
 # Downloading Latest fclone/gclone binary and adding to path
 echo
 cat << EOF
-1. Fclone
-2. Gclone
+1. fclone
+2. gclone
 EOF
 read -e -p "What would you like to install , Enter input as 1 or 2 : " opt
 echo
