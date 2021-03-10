807dd5843de83c400b885d115790585ead35c12c
xd003 <singhbadal04@gmail.com>
2021-03-10 22:35:14 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -164,14 +164,14 @@ case $opt in
     cecho b "lclone binary already exists in path // Skipping"
   fi
   if [ "$ehome" == "/data/data/com.termux/files/home" ]; then
-    sed -i '115d' $(which clone)
-    sed -i '121d' $(which clone)
-    sed -i '127d' $(which clone)
+    sed -i '115s|(cd $HOME/easyclone/sasync; bash sasync set.copy)|lclone --config=$conf copy src: dst: $uflags --ignore-existing|' $(which clone)
+    sed -i '121s|(cd $HOME/easyclone/sasync; bash sasync set.move)|lclone --config=$conf move src: dst: $uflags --delete-empty-src-dirs --ignore-existing|' $(which clone)
+    sed -i '127s|(cd $HOME/easyclone/sasync; bash sasync set.sync)|lclone --config=$conf sync src: dst: $uflags --ignore-existing|' $(which clone)
     sed -i "s|rclone|lclone|g" $(which clone)
   else
-    sudo sed -i '115d' $(which clone)
-    sudo sed -i '121d' $(which clone)
-    sudo sed -i '127d' $(which clone)
+    sudo sed -i '115s|(cd $HOME/easyclone/sasync; bash sasync set.copy)|lclone --config=$conf copy src: dst: $uflags --ignore-existing|' $(which clone)
+    sudo sed -i '121s|(cd $HOME/easyclone/sasync; bash sasync set.move)|lclone --config=$conf move src: dst: $uflags --delete-empty-src-dirs --ignore-existing|' $(which clone)
+    sudo sed -i '127s|(cd $HOME/easyclone/sasync; bash sasync set.sync)|lclone --config=$conf sync src: dst: $uflags --ignore-existing|' $(which clone)
     sudo sed -i "s|rclone|lclone|g" $(which clone)
   fi
   ;;
