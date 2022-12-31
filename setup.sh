3b075dd52e8d7bd625967306e11d1806f418a1d5
xd003 <singhbadal04@gmail.com>
2022-12-31 13:48:34 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -126,11 +126,11 @@ fi
 
 cecho g "¶ Downloading and adding gclone to path"
 egclone="$(gclone version 2>/dev/null)"
-check="$(echo "$egclone" | grep 'v1\.60\.1')"
+check="$(echo "$egclone" | grep 'v1\.61\.1')"
 if [ -z "${check}" ] ; then
-  gclone_version="1.60.1-fog"
+  gclone_version="1.61.1-cold"
   if [ "$arch" == "aarch64" ] ; then
-    URL=https://easyclone.xd003.workers.dev/0:/gclone/gclone-v1.60.1-fog-linux-aarch64.zip 
+    URL=https://easyclone.xd003.workers.dev/0:/gclone/gclone-v1.61.1-cold-linux-aarch64.zip 
   else
     URL=https://github.com/l3v11/gclone/releases/download/$gclone_version/gclone-$gclone_version-linux-$arch.zip
   fi
